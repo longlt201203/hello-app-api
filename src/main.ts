2f8f0d5719fe28354f09d7733d05236699e9c5ea
longlt201203 <longlt201203@nexondv.com>
2024-07-03 10:58:12 +0700

    feat: 🎸 initi

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { Env } from '@utils';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Env } from "@utils";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(Env.LISTEN_PORT);
+	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix(Env.GLOBAL_PREFIX);
+	const config = new DocumentBuilder()
+		.setTitle("Hello App")
+		.setDescription("Hello App API document")
+		.setVersion("1.0")
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api/docs", app, document);
+
+	await app.listen(Env.LISTEN_PORT);
 }
 bootstrap();
--- src/modules/sample/sample.module.ts
+++ src/modules/sample/sample.module.ts
@@ -1,12 +1,10 @@
-import { Module } from '@nestjs/common';
-import { SampleService } from './sample.service';
-import { SampleController } from './sample.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Sample } from '@db/entities';
+import { Module } from "@nestjs/common";
+import { SampleService } from "./sample.service";
+import { SampleController } from "./sample.controller";
 
 @Module({
-  controllers: [SampleController],
-  providers: [SampleService],
-  imports: [TypeOrmModule.forFeature([Sample])]
+	controllers: [SampleController],
+	providers: [SampleService],
+	imports: [],
 })
 export class SampleModule {}
