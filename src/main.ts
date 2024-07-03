dfa4775f198bf53d6ec5e31830724edf1019d373
longlt201203 <longlt201203@nexondv.com>
2024-07-03 12:42:13 +0700

    feat: 🎸 enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	app.enableCors({ origin: "*" });
 	app.setGlobalPrefix(Env.GLOBAL_PREFIX);
 	const config = new DocumentBuilder()
 		.setTitle("Hello App")
